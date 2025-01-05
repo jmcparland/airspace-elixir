e6816216754a34c3f2046068ad8b123e3b2f079c
Joe McParland <jmcparland@gmail.com>
2025-01-05 05:32:33 +0000

    checkpoint

--- flights/lib/airspace/flight.ex
+++ flights/lib/airspace/flight.ex
@@ -42,9 +42,42 @@ defmodule Airspace.Flight do
     new_state = Map.put(state, :vector, updated_vector)
     new_state = Map.put(new_state, :expire, Process.send_after(self(), :expire, 120_000))
 
+    new_state =
+      if not state.complete and complete?(updated_vector) do
+        GenServer.cast(Airspace.Reporter, {:report, inspect(updated_vector)})
+        Map.put(new_state, :complete, true)
+      else
+        new_state
+      end
+
     # GenServer.cast(Airspace.Reporter, {:report, inspect(updated_vector)})
 
     {:noreply, new_state}
+  end
+
+  defp complete?(vector) do
+    [
+      _,
+      _,
+      _,
+      _,
+      _,
+      _,
+      _,
+      _,
+      _,
+      _,
+      callsign,
+      altitude,
+      ground_speed,
+      track,
+      latitude,
+      longitude,
+      vertical_rate | _
+    ] = vector
+
+    callsign != "" && altitude != "" && ground_speed != "" && track != "" && latitude != "" &&
+      longitude != "" && vertical_rate != ""
   end
 
   defp cancel_expire_timer(ref) do
